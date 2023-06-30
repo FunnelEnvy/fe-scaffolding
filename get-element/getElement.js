176946a470b2d257e32fd5d9691ea21cf9381da1
maaislam <maaislam@outlook.com>
2023-06-30 08:39:59 +0600

    merge error fix

--- get-element/getElement.js
+++ get-element/getElement.js
@@ -1,11 +1,11 @@
 /**
     Returns a promise that resolves with an object containing all elements that match a given CSS selector. If no elements are found, it will wait for mutations on the document body and retry until elements matching the selector are found or a timeout is reached.
     @param {string} cssSelector - The CSS selector used to select elements to be found.
-    @param {number} [outTimer=10000] - The maximum time in milliseconds to wait for elements to be found. Defaults to 100000ms.
+    @param {number} [timeout=10000] - The maximum time in milliseconds to wait for elements to be found. Defaults to 100000ms.
     @returns {Promise} - A promise that resolves with an object containing the CSS selector and an array of found elements.
     @throws {Error} - If a timeout is reached before elements matching the selector are found.
     */
-const getElement = (cssSelector, onError = null, outTimer = 10000) => {
+const getElement = (cssSelector, onError = null, timeout = 10000) => {
   const els = document.querySelectorAll(cssSelector);
   if (els.length > 0) {
     return Promise.resolve({
@@ -37,7 +37,7 @@ const getElement = (cssSelector, onError = null, outTimer = 10000) => {
         onError(errorMessage);
       }
       reject(errorMessage);
-    }, outTimer);
+    }, timeout);
   });
 };
 
